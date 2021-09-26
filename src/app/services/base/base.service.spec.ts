469d98022de3af519e093ec9b343962266ea090f
Cauê Almeida <nncl@live.com>
2021-09-26 11:33:07 -0300

    test(services): Refactoring Base Service spec

--- src/app/services/base/base.service.spec.ts
+++ src/app/services/base/base.service.spec.ts
@@ -1,9 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BaseService } from './base.service';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+class BService extends BaseService<any, any, any> {
+  constructor(http: HttpClient) {
+    super(http, '', 'v1');
+  }
+}
+
+export interface Item {
+  name: string;
+  url: string;
+}
 
 describe('BaseService', () => {
-  let service: BaseService;
+  let service: BaseService<Item, Item, Item>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
