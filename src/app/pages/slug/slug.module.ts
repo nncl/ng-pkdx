0aaf186d556824c74bc89846363a42bb3d527ae7
Cauê Almeida <nncl@live.com>
2021-09-26 12:52:06 -0300

    feat(pages): Stylizing Details' page

--- src/app/pages/slug/slug.module.ts
+++ src/app/pages/slug/slug.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { SlugRoutingModule } from './slug-routing.module';
 import { SlugComponent } from './slug.component';
+import { CardModule } from '../../components/card/card.module';
 
 
 @NgModule({
   declarations: [
     SlugComponent
   ],
-  imports: [
-    CommonModule,
-    SlugRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        SlugRoutingModule,
+        CardModule
+    ]
 })
 export class SlugModule { }
