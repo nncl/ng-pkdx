23c99bd05b195b60048c3a377b50bbabb1271ea7
Cauê Almeida <nncl@live.com>
2021-09-26 14:01:09 -0300

    feat(pages): Getting Pokémon's details

--- src/app/pages/slug/slug.component.ts
+++ src/app/pages/slug/slug.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../../services/pokemon/pokemon.service';
+import { Observable } from 'rxjs';
+import { PokemonListItem } from '../../services/pokemon/models';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-slug',
   templateUrl: './slug.component.html',
-  styleUrls: ['./slug.component.scss']
+  styleUrls: [ './slug.component.scss' ]
 })
 export class SlugComponent implements OnInit {
+  $item: Observable<PokemonListItem> | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private location: Location,
+              private pokemonService: PokemonService) {
+
+    route.params.subscribe(({ slug }) => {
+      this.$item = pokemonService.getById(slug);
+    });
+
+  }
 
   ngOnInit(): void {
+  }
+
+  back(): void {
+    this.location.back();
   }
 
 }
